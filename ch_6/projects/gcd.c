0ec8af13eecf032a4e60c2960c4941434138cb1f
Madoche <84352397+madochelj@users.noreply.github.com>
2023-02-21 05:28:14 +0000

    gcd done

--- ch_6/projects/gcd.c
+++ ch_6/projects/gcd.c
@@ -1,16 +1,30 @@
 // author: mlj
-// purpose: find the greatest common factor with two inputs
-// date: 02/19/23
+// purpose: find the greatest common factor
+// date: 2/20/23
 
 #include <stdio.h>
 
-int main(void)
-{
-    int input_one;
-    int input_two;
+int main(void) {
 
-    printf("Enter two integers: ");
-    scanf("%d %d", &input_one, &input_two);
+    int input_1;
+    int input_2;
+    int temp_bin;
 
-    return 0;
+    // get two inputs from user
+    printf("Enter two integers (# #): ");
+    scanf("%d %d", &input_1, &input_2);
+    
+    // using the Euclidean Algorithm to find the greatest common factor
+    // loops break on find the solution
+    for (;;) {
+        // a, b, c, a / b = c, b / c = (c)
+        temp_bin = input_1 % input_2;
+        input_1 = input_2;
+        input_2 = temp_bin;
+
+        if (input_2 == 0)
+            break;
+    }
+
+    printf("%d\n", input_1);
 }
